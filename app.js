ef1766fa25355104d0856614fab752fd6aaeceb2
Marijke van der Veer <mjvanderveer@gmail.com>
2017-02-06 17:50:29 +0100

    working on post request..

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // import the necessary libraries/declare the necessary objects
 var express = require('express'); // load the express library and store it into a variable called 'express'
 var pug = require('pug'); // load the pug library, and store it into a variable called 'pug'
+var router = express.Router(); // To make use of Express' routing capabilities you need to initiate a new Express Router.
 var bodyparser = require('body-parser') //bodyparser is necessary for the middleware
 var searchRoutes = require(__dirname + '/routes/searchRoutes'); // load the searchRoutes.js file, and store it into a variable called 'searchRoutes'
 
@@ -10,9 +11,12 @@ var app = express(); // call the express function, and store it into a variable 
 // view engine setup
 app.set('view engine', 'pug'); // pug is defined as the view engine in the express application
 
+// tell Express to use your router
+app.use(router);
 
 //for all the routing make use of the bodyparser --> Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST) and exposes the resulting object (containing the keys and values) on req.body. 
 app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
 // app.use('/search', searchRoutes;)
 
 app.get('/', (request, response) => {
--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -1,12 +1,26 @@
-var express = require('express');
-// To make use of Express' routing capabilities you need to initiate a new Express Router.
-var router = express.Router();
-var parsedUsers = require('./../userreader');
+// CREATE THE ROUTE
 
+var express = require('express');
 var app = express();
 
+// To make use of Express' routing capabilities you need to initiate a new Express Router.
+var router = express.Router();
 // tell Express to use your router
 app.use(router);
+
+var bodyparser = require('body-parser')
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
+
+var parsedUsers = require('./../userreader');
+
+// post results in new page searchResults.pug
+app.post('/searchResults', function (request, response) {
+	var searchQuery = request.body.name;
+	// response.end('Search Query: ' + search)
+	console.log(searchQuery)
+});
+
 
 
 
