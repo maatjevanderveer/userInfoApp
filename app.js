66102dc714f163993f25a302cdfea6bc9c9e9a82
Marijke van der Veer <mjvanderveer@gmail.com>
2017-02-06 11:45:34 +0100

    added search.js, searchResults.pug file and adjusted file structure in directory

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
+// import the necessary libraries/declare the necessary objects
 var express = require('express'); // load the express library and store it into a variable called 'express'
 var pug = require('pug'); // load the pug library, and store it into a variable called 'pug'
 var bodyparser = require('body-parser') //bodyparser is necessary for the middleware
+var searchRoutes = require(__dirname + '/routes/search'); // load the search.js file, and store it into a variable called 'searchRoutes'
 
 var parsedUsers = require(__dirname + '/userreader'); // load the userreader.js file, and store it into a variable called 'parsedJSON'
-
 var app = express(); // call the express function, and store it into a variable called 'app'
 
 // view engine setup
 app.set('view engine', 'pug'); // pug is defined as the view engine in the express application
 
-//for all the routing make use of the bodyparser
-app.use(bodyparser());
+//for all the routing make use of the bodyparser --> Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST) and exposes the resulting object (containing the keys and values) on req.body. 
+app.use(bodyparser.urlencoded({extended: false}));
+app.use('/search', searchRoutes;)
 
 app.get('/', (request, response) => {
 	response.render('index', {users: parsedUsers.getUsers() }); //render is how we take a pug template and render it as normal HTML
 });
+
 // render a page, that displays a form which is the search bar
 app.get('/search', (request, response) => {
 	response.render('search')
 });
+
 //render a page that displays a registration form for new users
 app.get('/register', (request, response) => {
 	response.render('register')
@@ -26,7 +30,7 @@ app.get('/register', (request, response) => {
 
 // takes in the post request from your search bar.
 app.post('/search', (request, response) => {
-	var searchQuery = 
+	var searchQuery = request.body.search
 	// receive the form data and store in variable
 });
 
