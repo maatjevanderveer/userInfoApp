91ac5102f2c9a82f8780279c62abd30813daaabd
Marijke van der Veer <mjvanderveer@gmail.com>
2017-02-07 16:23:13 +0100

    added searchbar which gives a result

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 // import the necessary libraries/declare the necessary objects
 var express = require('express'); // load the express library and store it into a variable called 'express'
-var pug = require('pug'); // load the pug library, and store it into a variable called 'pug'
-var router = express.Router(); // To make use of Express' routing capabilities you need to initiate a new Express Router.
 var bodyparser = require('body-parser') //bodyparser is necessary for the middleware
-var searchRoutes = require(__dirname + '/routes/searchRoutes'); // load the searchRoutes.js file, and store it into a variable called 'searchRoutes'
-
-var parsedUsers = require(__dirname + '/userreader'); // load the userreader.js file, and store it into a variable called 'parsedJSON'
 var app = express(); // call the express function, and store it into a variable called 'app'
+var fs = require('fs') // load the fs (filesystem) library for the readFile() method
 
 // view engine setup
 app.set('view engine', 'pug'); // pug is defined as the view engine in the express application
-
-// tell Express to use your router
-app.use(router);
+app.set('views', './views') // this defaults to the view directory in the application root directory
 
 //for all the routing make use of the bodyparser --> Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST) and exposes the resulting object (containing the keys and values) on req.body. 
 app.use(bodyparser.urlencoded({extended: false}));
 app.use(bodyparser.json());
-// app.use('/search', searchRoutes;)
 
 app.get('/', (request, response) => {
-	response.render('index', {users: parsedUsers.getUsers() }); //render is how we take a pug template and render it as normal HTML
+	console.log('de / is nu getriggered')
+	fs.readFile('./users.json', (error, data) => { 	//stap 1. zorg dat je users.json inleest
+		if (error) {
+			throw error
+		};
+		console.log('file is read');
+		var parsedUsers = JSON.parse(data); // parse users.json file 
+		response.render('index', {parsedUsers: parsedUsers}); 	//render is how we take a pug template and render it as normal HTML
+	});															// the second parameter of .render() MUST be an object --> {<randomkeyname>: parsedUsers} here, value is the array with al the users
 });
 
 // render the search.pug file and send it to the browser (displays a form which is the search bar)
@@ -33,11 +34,25 @@ app.get('/register', (request, response) => {
 	response.render('register')
 });
 
-// takes in the post request from your search bar.
-// app.post('/search', (request, response) => {
-// 	var searchQuery = request.body.search
-// 	// receive the form data and store in variable
-// });
+// post results in new page searchResults.pug
+app.post('/search', function (request, response) {
+	var searchQuery = request.body.searchquery; //request the value written in the searchbar and store it into var
+
+	fs.readFile('./users.json', (error, data) => { 	//lees users.json in
+		if (error) {
+			throw error
+		};
+		var parsedUsers = JSON.parse(data); // parse users.json file 
+
+		for(var i=0; i < parsedUsers.length; i++) {
+			if (searchQuery === parsedUsers[i].firstname || searchQuery === parsedUsers[i].lastname || searchQuery === parsedUsers[i].email){
+				console.log('found!')
+				console.log(parsedUsers[i])
+				response.render('searchResults', {parsedUser: parsedUsers[i]})
+			}
+		};
+	});
+});
 
 // takes in the post request from your register form.
 app.post('/register', (request, response) => {
